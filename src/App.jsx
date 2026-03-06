b7d8445e19f92f95c88a71313c4781e4b6644325
Lakshmi--git <sankarlachu6810@gmail.com>
2026-03-06 21:21:59 +0530

    noscript

--- src/App.jsx
+++ src/App.jsx
@@ -26,3 +26,79 @@ function App() {
 }
 
 export default App;
+
+import { useState } from "react";
+
+function App() {
+
+  const [books, setBooks] = useState([]);
+
+  function getBooks() {
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(
+
+        function(position) {
+
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+
+          fetch(`https://peedika-1.onrender.com?lat=${lat}&lng=${lng}`)
+            .then(res => res.json())
+            .then(data => setBooks(data));
+
+        },
+
+        function() {
+
+          const district = "Kottayam";
+
+          fetch(`https://peedika-1.onrender.com??district=${district}`)
+            .then(res => res.json())
+            .then(data => setBooks(data));
+
+        }
+
+      );
+
+    } else {
+      console.log("Geolocation not supported");
+    }
+
+  }
+
+  return (
+
+    <div>
+
+      <h2>Nearby Books</h2>
+
+      <button onClick={getBooks}>
+        Find Nearby Books
+      </button>
+
+      {books.map((book, index) => {
+
+        const distanceText = book.distance
+          ? (book.distance / 1000).toFixed(1) + " km away"
+          : "";
+
+        return (
+          <div key={index} className="book-card">
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <p>{book.district}</p>
+            <span>{distanceText}</span>
+          </div>
+        )
+
+      })}
+
+    </div>
+
+  )
+
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,79 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-function getBooks() {
-
-  console.log("running");
-
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-function getBooks() {
-
-  if (navigator.geolocation) {
-
-    navigator.geolocation.getCurrentPosition(
-
-      function(position) {
-
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-
-        fetch(`http://localhost:5000/books?lat=${lat}&lng=${lng}`)
-          .then(res => res.json())
-          .then(data => displayBooks(data));
-
-      },
-
-      function() {
-
-        const district = "Kottayam";
-
-        fetch(`http://localhost:5000/books?district=${district}`)
-          .then(res => res.json())
-          .then(data => displayBooks(data));
-
-      }
-
-    );
-
-  } else {
-
-    console.log("Geolocation not supported");
-
-  }
-
-}
-
-function displayBooks(books){
-
-  const container = document.getElementById("books");
-
-  container.innerHTML = "";
-
-  books.forEach(book => {
-
-    const distanceText = book.distance
-      ? (book.distance / 1000).toFixed(1) + " km away"
-      : "";
-
-    const card = `
-      <div class="book-card">
-        <h3>${book.title}</h3>
-        <p>${book.author}</p>
-        <p>${book.district}</p>
-        <span>${distanceText}</span>
-      </div>
-    `;
-
-    container.innerHTML += card;
-
-  });
-
-}
-
-window.onload = getBooks;
