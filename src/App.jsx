74545089eb2ac17cef9fa7f9200fd761f346fd3e
Lakshmi--git <sankarlachu6810@gmail.com>
2026-03-11 14:56:06 +0530

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import Home from "./pages/Home";
 import SellBook from "./pages/SellBook";
 import Login from "./pages/Login";
@@ -8,6 +9,14 @@ import BookDetails from "./pages/BookDetails";
 function App() {
 
   const [books, setBooks] = useState([]);
+
+  // Load books when app starts
+  useEffect(() => {
+    fetch("https://peedika-1.onrender.com/books")
+      .then(res => res.json())
+      .then(data => setBooks(data))
+      .catch(err => console.error(err));
+  }, []);
 
   const addBook = (newBook) => {
     setBooks((prevBooks) => [newBook, ...prevBooks]);
